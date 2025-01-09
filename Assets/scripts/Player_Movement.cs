5f8df08ef223a7bb7b079623ffb6bd069d5a3cb2
arsenijtelpuk8@gmail.com <arsenijtelpuk8@gmail.com>
2025-01-09 22:43:49 +0300

    Управление персонажем

--- Assets/scripts/Player_Movement.cs
+++ Assets/scripts/Player_Movement.cs
@@ -2,23 +2,27 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
     [SerializeField] int speed = 100;
+    private NavMeshAgent agent;
     private Rigidbody rb;
     Vector3 dir = new Vector3();
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateUpAxis = false;
+        agent.updateRotation = false;
     }
     private void Update()
     {
-        dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
-            rb.MovePosition(new Vector3(dir.x, dir.y, 0));
-
+            dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+        agent.SetDestination(dir);
     }
 }
